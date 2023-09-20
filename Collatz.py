a604a3399d170f7307a180cce8fa3edde8ea0c36
DiegoRodriguez25 <diego2222621@correo.uis.edu.co>
2023-09-20 08:59:10 -0500

    CODIGO COLLATZ

---
+++ Collatz.py
@@ -0,0 +1,38 @@
+
+lista = []
+
+digito = int(input("\n\nIngrese el último dígito de su código: "))
+a = digito + 3
+
+for i in range(1,a+1):
+    listap = int(input("Ingrese un numero"))
+    lista.append(listap)
+    
+nump = sum(lista)/a
+
+numero = int(nump)
+
+print("El número promeido es:",numero)
+
+while numero != 1:
+     
+    if numero %2==0: 
+         numero = (numero/2)
+         print("\nSe divide x2 y da: ", numero)
+         
+    else: 
+        numero = (numero*3)+1
+        print("\nSe multiplica por 3 y se le suma uno y da: ", numero)
+        
+print("\n\nSE HA CUMPLIDO LA SECUENCIA DE COLLATZ")
+
+
+
+        
+    
+        
+        
+         
+
+         
+         
